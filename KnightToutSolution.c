252b4de880078d407ce0958b6c0e01de038c0229
Andres R <xnorlogic@gmail.com>
2017-08-17 14:42:12 -0500

    Create KnightToutSolution.c

---
+++ KnightToutSolution.c
@@ -0,0 +1,70 @@
+/*
+	Solves (partialy) the Knight Tour Problem from the 64 start location posibilities.
+	by AndyR.
+*/
+
+#include "knightour.h"
+
+//Structure to hold location of particular piece on the board... 
+//This is the White Knight 1
+Chess_Piece_Location White_Knight_1_Location;
+
+int main(){
+
+	char Location_Solution[] = "KnightTourOutput.txt";
+	char Location_Moves[] = "MovesStatus.txt";
+
+	int Board [ROW][COL];
+	int NumberOfMoves = 0;
+	int BestNextMove;
+	
+	FILE *fp;
+    fp = fopen(Location_Moves, "a");
+	
+	for (int LOOPx=0;LOOPx<8;LOOPx++){	
+		for (int LOOPy=0;LOOPy<8;LOOPy++){
+		
+			fprintf(fp,"\n ------Loop: %d ------",LOOPy);
+			
+			//Initialize the next move to 0 (no move at the begining)
+			BestNextMove = 0;
+			//Clear the board (Clean zero no moves on the board)
+			ClearBoard(Board);
+			
+			//Seed the initial or starting point of the Knight
+			White_Knight_1_Location.x = LOOPx;
+			White_Knight_1_Location.y = LOOPy;
+			
+			for (int CNT=1;CNT<65;CNT++){	
+				
+				//Perform a Move
+				PerformMove(White_Knight_1_Location.x, White_Knight_1_Location.y, BestNextMove, CNT, Board);
+				
+				//Relocate the Knight
+				White_Knight_1_Location.x = White_Knight_1_Location.x;
+				White_Knight_1_Location.y = White_Knight_1_Location.y;
+				
+				//Determine number of moves available from the current location of the Knight
+				NumberOfMoves = DetermineMoves(White_Knight_1_Location.x, White_Knight_1_Location.y, Board);
+				//Determine the best next move for the Knight
+				BestNextMove = NextMove(White_Knight_1_Location.x, White_Knight_1_Location.y, Board);
+				
+				//Do some print outs to the file
+				fprintf(fp,"\n Jump : %d ",CNT);
+				fprintf(fp,"\n Knight Location: %d,%d ",White_Knight_1_Location.x,White_Knight_1_Location.y);
+				fprintf(fp,"\n Available moves: %d ",NumberOfMoves);
+				fprintf(fp,"\n Best Next Move is : %d \n ",BestNextMove);	
+				
+			}
+			
+			//Write the final board to the file
+			WriteToFile(Board,Location_Solution);	
+		}
+		fprintf(fp,"\n --------------------- \n");
+	}
+	
+	fclose(fp);
+	printf("\n Solutions Completed \n ");
+	
+	return 0;
+}
